cca618cf47d3a400d26980d6396cad9bb373a969
Александр <46243492+jswift911@users.noreply.github.com>
2023-03-06 12:22:39 +0300

    Update RestServiceTest.java

--- src/test/java/ru/netology/services/RestServiceTest.java
+++ src/test/java/ru/netology/services/RestServiceTest.java
@@ -2,7 +2,9 @@ package ru.netology.services;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
+
 public class RestServiceTest {
+    
     @ParameterizedTest
     @CsvFileSource(files = "src/test/resources/rest.csv")
     public void shouldCalcMonthsExact(int expected, int income, int expenses, int threshold) {
